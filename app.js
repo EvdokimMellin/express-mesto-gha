f9ca23d061aaa8b888419b09e3ffed72dc9f2536
evdoc <evdockimpyatkov.ru@yandex.ru>
2023-02-09 05:53:03 +0300

    fix: больше исправлений для тестов

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { celebrate, Joi } = require('celebrate');
 
 const app = express();
 
@@ -17,8 +18,21 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().min(2).pattern(/https?:\/\/[\S]+/),
+  }),
+}), createUser);
 
 app.use(auth);
 
